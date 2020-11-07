b58696a32f9cbe4ee57bb00f1dc25fb9681c30d7
Code-Apprentice <codeguru42@gmail.com>
2020-11-07 14:41:15 -0700

    Move fab to list fragment

--- app/src/main/java/codeguru/jobsearch/JobsListFragment.kt
+++ app/src/main/java/codeguru/jobsearch/JobsListFragment.kt
@@ -1,12 +1,12 @@
 package codeguru.jobsearch
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,7 +24,7 @@ class JobsListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
