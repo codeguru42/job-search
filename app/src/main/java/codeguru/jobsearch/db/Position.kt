2ea7632601f79f9006af6d95d825c6706ab5c76f
Code-Apprentice <codeguru42@gmail.com>
2020-11-08 10:19:54 -0700

    Position entity

---
+++ app/src/main/java/codeguru/jobsearch/db/Position.kt
@@ -0,0 +1,12 @@
+package codeguru.jobsearch.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Position(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var businessName: String
+)
