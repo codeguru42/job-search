5118238066de5ed449f389cee3d9428df954d586
Code-Apprentice <codeguru42@gmail.com>
2020-12-27 12:30:34 -0700

    Flesh out test for Position Details

--- app/src/androidTest/java/codeguru/jobsearch/ui/TestPositionDetails.kt
+++ app/src/androidTest/java/codeguru/jobsearch/ui/TestPositionDetails.kt
@@ -1,7 +1,12 @@
 package codeguru.jobsearch.ui
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import codeguru.jobsearch.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -9,6 +14,15 @@ import org.junit.runner.RunWith
 class TestPositionDetails {
     @Test
     fun testCreatePosition() {
-        launchFragmentInContainer<PositionDetails>()
+        val scenario = launchFragmentInContainer<PositionDetails>()
+        scenario.moveToState(Lifecycle.State.STARTED)
+        onView(withId(R.id.text_title))
+            .perform(typeText("Software Engineer"))
+        onView(withId(R.id.text_business_name))
+            .perform(
+                typeText("Google"),
+                closeSoftKeyboard()
+            )
+        onView(withId(R.id.position_save)).perform(click())
     }
 }
