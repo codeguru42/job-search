d204ecbc77fca3e82225ffc5c3bee9ca896556e8
Code-Apprentice <codeguru42@gmail.com>
2023-08-27 10:46:17 -0600

    Add compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,15 @@ dependencies {
 
     // Navigation testing
     androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // Jetpack Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
