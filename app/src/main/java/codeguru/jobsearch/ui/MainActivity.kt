7f1daafa5febd9d89ba2c808ac6d71eb032a83cc
Code-Apprentice <codeguru42@gmail.com>
2023-09-02 15:40:01 -0600

    Remove menu code

--- app/src/main/java/codeguru/jobsearch/ui/MainActivity.kt
+++ app/src/main/java/codeguru/jobsearch/ui/MainActivity.kt
@@ -1,8 +1,6 @@
 package codeguru.jobsearch.ui
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
@@ -32,22 +30,6 @@ class MainActivity : ComponentActivity() {
             JobSearchDatabase::class.java,
             "job_search"
         ).build()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
 
